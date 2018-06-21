4c7888fca5597734d873900be43dc5c9d7733dd2
wali <386200854@qq.com>
2018-06-21 10:50:15 +0800

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  title: 'Hello VuePress',
+  description: 'Just playing around',
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Learn', link: '/learn/' },
+      { text: 'External', link: 'https://google.com' },
+      {
+        text: 'Languages',
+        items: [
+          { text: 'Chinese', link: '/language/chinese' },
+          { text: 'Japanese', link: '/language/japanese' }
+        ]
+      }
+    ]
+  }
+};
---
+++ docs/learn/README.md
@@ -0,0 +1,6 @@
+## 文档
+- 文件夹都应当有一个 README.md 文件，它会被自动编译为 index.html。
+    - 确保链接以 `.html` 或 `.md` 结尾；
+    - 确保路径大小写正确，因为路径的匹配是大小写敏感的;
+    - /config/ 表示的是/config目录下的README.md文件，而 /config指的是/config.md
+    
